5c43c9034c0f4c6566e7442d438bcf08fad145df
joe <bigod26@gmail.com>
2016-07-24 07:00:02 +0000

    (fix) race condition in example, breaking tests

--- test/examples/api/async.js
+++ test/examples/api/async.js
@@ -6,20 +6,21 @@ module.exports = [
         expect(app).to.not.be.null;
 
         var dump = app.tasks.logger.getBuffer();
-        // console.info('async logger dump:', dump);
+        console.info('async logger dump:', dump);
 
         var expectList = [
             'MyTasks task1: before',
             'MyTasks task1: promise delay 200',
             'MyTasks task1: after',
             'MyTasks task2: before',
-            'MyTasks task2: yield delay 200',
+            'MyTasks task2: yield delay 300',
             'MyTasks task2: after',
+            
             'MyTasks task1: before',
             'MyTasks task2: before',
             'MyTasks task1: promise delay 200',
-            'MyTasks task2: yield delay 200',
             'MyTasks task1: after',
+            'MyTasks task2: yield delay 300',
             'MyTasks task2: after'
         ];
 
