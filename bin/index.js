c20b3dbd617d77e352f4041a3202060e64ebf86f
dfatkhi <dfatkhi@cisco.com>
2016-11-04 14:28:46 -0700

    prevent global bz creating its own instance

--- bin/index.js
+++ bin/index.js
@@ -1,3 +1,9 @@
-var Beelzebub  = require('../index.js');
+var path = require('path');
+try {
+  var Beelzebub  = require(path.join(process.cwd(), 'node_modules/beelzebub'));
+}
+catch(e) {
+  throw Error('Beelzebub should be installed both locally and globally');
+}
 var cli = new Beelzebub.CLI();
 cli.run();
